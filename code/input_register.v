c0536d61175c096e1dda9e5df47cacdba93f82f1
EliezerGreen <136101268+EliezerGreen@users.noreply.github.com>
2023-06-10 21:51:34 +0300

    Update input_register.v
    synchronization

--- code/input_register.v
+++ code/input_register.v
@@ -4,18 +4,19 @@ module input_register
     input  wire        reset,
     input  wire [63:0] data_in,
     input  wire        load,
-    output wire [63:0] plaintext
+    output reg  [63:0] plaintext
   );
 
 reg [63:0] data;
 
-always @(reset or posedge clk)
+always @(posedge clk)
   if (reset)
     plaintext <= 64'b0;
   else
     if (load == 1'b1)
       data <= data_in;
-	  
-assign plaintext = data;
+
+always @(posedge clk)
+  assign plaintext <= data;
 
 endmodule
